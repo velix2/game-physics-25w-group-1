e1e887a2b05d03a4e1c912a7aa90e0924fbfad40
Felix Daxenberger <136970774+velix2@users.noreply.github.com>
2025-12-16 10:37:58 +0100

    refactor everyting

--- Scenes/Common.cpp
+++ Scenes/Common.cpp
@@ -1,10 +1,28 @@
 #include "Common.h"
 
-void ExplicitStep(const std::vector<std::vector<float>> &current_temp_field, std::vector<std::vector<float>> &updated_temp_field, int i, int j, float thermal_diffusivity, float delta_x, float delta_y, float delta_t)
+void explicitStep(TempField &temp_field, float delta_t)
+{
+    // New field
+    auto next_field = std::vector<std::vector<float>>(temp_field.getN(), std::vector<float>(temp_field.getM(), 0.0f));
+
+    // Iterate over entire field
+    for (int i = 0; i < temp_field.getN(); i++)
+    {
+        for (int j = 0; j < temp_field.getM(); j++)
+        {
+            explicitStepHelper(temp_field, next_field, i, j, delta_t);
+        }
+    }
+
+    // Apply new field
+    temp_field.setTempField(std::move(next_field));
+}
+
+void explicitStepHelper(TempField &current_temp_field, std::vector<std::vector<float>> &updated_temp_field, int i, int j, float delta_t)
 {
     // Check for boundary conditions
-    auto x_len = current_temp_field.size();
-    auto y_len = current_temp_field[0].size();
+    auto x_len = current_temp_field.getN();
+    auto y_len = current_temp_field.getM();
 
     float T_center = current_temp_field[i][j];
     float T_x_dir_prev, T_x_dir_next, T_y_dir_prev, T_y_dir_next;
@@ -14,11 +32,54 @@ void ExplicitStep(const std::vector<std::vector<float>> &current_temp_field, std
     T_y_dir_prev = 0 < i ? current_temp_field[i][j - 1] : 0;
     T_y_dir_next = i < y_len - 1 ? current_temp_field[i][j + 1] : 0;
 
+    auto delta_x = current_temp_field.deltaX();
+    auto delta_y = current_temp_field.deltaY();
+
     auto d_dx2 = (T_x_dir_next - 2.0f * T_center + T_x_dir_prev) / (delta_x * delta_x);
     auto d_dy2 = (T_y_dir_next - 2.0f * T_center + T_y_dir_prev) / (delta_y * delta_y);
 
-    auto delta_change = thermal_diffusivity * (d_dx2 + d_dy2);
+    auto delta_change = current_temp_field.getThermalDiffusivity() * (d_dx2 + d_dy2);
 
     auto new_temp = delta_change * delta_t + T_center;
     updated_temp_field[i][j] = new_temp;
+}
+
+TempField::TempField(float thermal_diffusivity, std::vector<std::vector<float>> initial_temp_field, float x_domain_lower, float x_domain_upper, float y_domain_lower, float y_domain_upper)
+    : thermal_diffusivity(thermal_diffusivity),
+      temp_field(std::move(initial_temp_field)),
+      x_domain_lower(x_domain_lower),
+      x_domain_upper(x_domain_upper),
+      y_domain_lower(y_domain_lower),
+      y_domain_upper(y_domain_upper)
+{
+}
+
+TempField::TempField(float thermal_diffusivity, int n, int m, float x_domain_lower, float x_domain_upper, float y_domain_lower, float y_domain_upper)
+    : thermal_diffusivity(thermal_diffusivity),
+      temp_field(std::vector<std::vector<float>>(n, std::vector<float>(m, 0.0f))),
+      x_domain_lower(x_domain_lower),
+      x_domain_upper(x_domain_upper),
+      y_domain_lower(y_domain_lower),
+      y_domain_upper(y_domain_upper)
+{
+}
+
+std::vector<float> &TempField::operator[](int i)
+{
+    return this->temp_field[i];
+}
+
+float TempField::deltaX()
+{
+    return (x_domain_upper - x_domain_lower) / n;
+}
+
+float TempField::deltaY()
+{
+    return (y_domain_upper - y_domain_lower) / m;
+}
+
+int TempField::totalSize()
+{
+    return this->n * this->m;
 }
--- Scenes/Common.h
+++ Scenes/Common.h
@@ -4,6 +4,72 @@
 #include <glm.hpp>
 #include <vector>
 
-void ExplicitStep(const std::vector<std::vector<float>> &current_temp_field, std::vector<std::vector<float>> &updated_temp_field, int i, int j, float thermal_diffusivity, float delta_x, float delta_y, float delta_t);
+class TempField
+{
+
+private:
+    std::vector<std::vector<float>> temp_field;
+    float thermal_diffusivity;
+    int n, m;
+    float x_domain_lower, x_domain_upper;
+    float y_domain_lower, y_domain_upper;
+
+public:
+    /**
+     * @brief Constructs a TempField object with a given thermal diffusivity, initial temperature field, and domain bounds.
+     *
+     * @param thermal_diffusivity The thermal diffusivity constant for the simulation.
+     * @param initial_temp_field 2D vector representing the initial temperature distribution.
+     * @param x_domain_lower Lower bound of the x-domain.
+     * @param x_domain_upper Upper bound of the x-domain.
+     * @param y_domain_lower Lower bound of the y-domain.
+     * @param y_domain_upper Upper bound of the y-domain.
+     */
+    TempField::TempField(float thermal_diffusivity, std::vector<std::vector<float>> initial_temp_field, float x_domain_lower, float x_domain_upper, float y_domain_lower, float y_domain_upper);
+
+    /**
+     * @brief Constructs a TempField object with a given thermal diffusivity, grid size, and domain bounds.
+     *        The temperature field is initialized to zero.
+     *
+     * @param thermal_diffusivity The thermal diffusivity constant for the simulation.
+     * @param n Number of grid points in the x-direction.
+     * @param m Number of grid points in the y-direction.
+     * @param x_domain_lower Lower bound of the x-domain (inclusive).
+     * @param x_domain_upper Upper bound of the x-domain (exclusive).
+     * @param y_domain_lower Lower bound of the y-domain (inclusive).
+     * @param y_domain_upper Upper bound of the y-domain (exclusive).
+     */
+    TempField(float thermal_diffusivity, int n, int m, float x_domain_lower, float x_domain_upper, float y_domain_lower, float y_domain_upper);
+
+    std::vector<float> &operator[](int);
+
+    float deltaX();
+    float deltaY();
+
+    int totalSize();
+
+    // Getters
+    const std::vector<std::vector<float>> &getTempField() const { return temp_field; }
+    float getThermalDiffusivity() const { return thermal_diffusivity; }
+    int getN() const { return n; }
+    int getM() const { return m; }
+    float getXDomainLower() const { return x_domain_lower; }
+    float getXDomainUpper() const { return x_domain_upper; }
+    float getYDomainLower() const { return y_domain_lower; }
+    float getYDomainUpper() const { return y_domain_upper; }
+
+    // Setters
+    void setTempField(const std::vector<std::vector<float>> &field) { temp_field = field; }
+    void setThermalDiffusivity(float diffusivity) { thermal_diffusivity = diffusivity; }
+    void setN(int value) { n = value; }
+    void setM(int value) { m = value; }
+    void setXDomainLower(float value) { x_domain_lower = value; }
+    void setXDomainUpper(float value) { x_domain_upper = value; }
+    void setYDomainLower(float value) { y_domain_lower = value; }
+    void setYDomainUpper(float value) { y_domain_upper = value; }
+};
+
+void explicitStep(TempField &temp_field, float delta_t);
+void explicitStepHelper(TempField &current_temp_field, std::vector<std::vector<float>> &updated_temp_field, int i, int j, float delta_t);
 
 #endif
